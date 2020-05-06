4975fce3a6f57598bbd994e57bed3d3a886bb679
Anastasia Kuznetsova <menina.indigena.17@gmail.com>
2020-05-06 14:47:56 -0400

    add constructor

--- main.cpp
+++ main.cpp
@@ -9,11 +9,11 @@ using namespace std;
 
 class Triplet{
     public:
-        char letter;
-        char target;
+        string letter;
+        string target;
         float weight;
 
-    Triplet(char letter, char target, float weight){
+    Triplet(string letter, string target, float weight){
         letter = letter;
         target = target;
         weight = weight;
@@ -24,18 +24,18 @@ class State{
     public:
         bool final;
         float finalweight;
-        map <char, vector<Triplet>> transitionsin; 
-        map <char, vector<Triplet>> transitionsout;
+        map <string, vector<Triplet>> transitionsin; 
+        map <string, vector<Triplet>> transitionsout;
 
-    void add_transition(char target, char input, char output, float weight){
+    void add_transition(string target, string input, string output, float weight){
         if (transitionsin.find(input) == transitionsin.end()){
             vector<Triplet> triplets;
-            transitionsin.insert(input, triplets);            
+            transitionsin[input]= triplets;            
         } 
 
         if (transitionsout.find(output) == transitionsout.end()){
             vector<Triplet> triplets;
-            transitionsout.insert(output, triplets);            
+            transitionsout[output] = triplets;            
         }
 
         Triplet t_in = Triplet(output, target, weight);
@@ -45,7 +45,7 @@ class State{
         transitionsin.find(output)->second.push_back(t_out);
     }
 
-    vector<Triplet> get_transitions(char symbol, string dir){
+    vector<Triplet> get_transitions(string symbol, string dir){
         if (dir.compare("down")==0){
             if (transitionsin.find(symbol)==transitionsin.end()){
                 vector<Triplet> v;
@@ -74,10 +74,58 @@ class ATTFST{
         string epsilon_symbol;
         string identity_symbol;
         string unknown_symbol;
+        map<int, State> states;
+        set<string> alphabet;
+
+    ATTFST(char* filename, string epsilon_symbol, string identity_symbol, string unknown_symbol){
+        epsilon_symbol = epsilon_symbol;
+        identity_symbol = identity_symbol;
+        unknown_symbol = unknown_symbol;
+        
+        //read lines from att file
+        vector<string> lines = read_lines(filename);
+
+        for(auto i = lines.begin(); i<lines.end(); i++){
+            cout << *i << endl;
+        }
+    }
+
+    vector<string> read_lines(char* filename){
+        char *cvalue = NULL;
+        int c;
+
+        FILE *f = fopen(filename, "r");		
+        c = fgetc(f); // get one character from the input
+
+        vector<string> lines;
+        string line;
+
+        while(c != EOF){	
+        if((char)c !='\n'){ 
+            line+=(char) c;
+            }else{
+                lines.push_back(line);
+                line="";                
+            }
+        c = fgetc(f);	
+        } 
+        return lines;
+    }
+
+    string map_syms(string s){
+        if(s.compare(epsilon_symbol)==0){
+            return "";
+        }
+        return s;
+    }
+
+};
+
+
+
+int main(int argc, char **argv){
+    ATTFST fst = ATTFST(argv[1], "", "", "");
+    //vector<string> v = fst.read_lines(argv[1]);
+    
+	return 0; 
 }
-
-int main()
-{
-
-return 0;
-} 
